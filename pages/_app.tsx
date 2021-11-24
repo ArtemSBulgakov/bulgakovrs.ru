8e54179cfa156b29d21cdec784ffb3008391db46
Artem Bulgakov <ArtemSBulgakov@ya.ru>
2021-11-24 18:03:37 +0300

    Add shrink-to-fit=no

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <meta name="viewport" content="width=device-width,initial-scale=1.0" />
+        <meta name="viewport" content="width=device-width,initial-scale=1.0,shrink-to-fit=no" />
       </Head>
       <Component {...pageProps} />
     </>
